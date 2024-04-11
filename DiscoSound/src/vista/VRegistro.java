2d932fc1dc1abdb17866900e4c50b751531a01dd
markel <markelarabiourrutia@gmail.com>
2024-04-11 09:34:55 +0200

    subidaRetoBase

--- DiscoSound/src/vista/VRegistro.java
+++ DiscoSound/src/vista/VRegistro.java
@@ -1,20 +1,23 @@
-package ventanadisco;
+package vista;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import java.awt.Font;
-import javax.swing.JLabel;
-import javax.swing.JCheckBox;
-import javax.swing.JButton;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 
-public class registro extends JFrame {
+import modelo.Dao;
 
+public class VRegistro extends JDialog {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JTextField textField_6;
@@ -23,27 +26,11 @@ public class registro extends JFrame {
 	private JTextField textField_3;
 	private JTextField textField_4;
 	private JTextField textField_5;
+	private Dao dao;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					registro frame = new registro();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public registro() {
+	private static final long serialVersionUID = 1L;
+	public VRegistro(Dao dao) {
+		this.dao=dao;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 900, 645);
 		contentPane = new JPanel();
@@ -169,4 +156,5 @@ public class registro extends JFrame {
 		btnSiguiente.setBounds(775, 577, 101, 21);
 		contentPane.add(btnSiguiente);
 	}
+
 }
