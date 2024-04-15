5caac5dc3fa7a80829e102ef4214307c2304392c
Sachinx23 <sachinbaruwal123@gmail.com>
2024-04-15 10:22:02 +0200

    subidaclases

---
+++ DiscoSound_Clases/src/clases/Administrador.java
@@ -0,0 +1,17 @@
+package clases;
+
+public class Administrador {
+	
+	//Atributo
+	private String nombreDiscoteca;
+
+	//Getters y setters 
+	public String getNombreDiscoteca() {
+		return nombreDiscoteca;
+	}
+
+	public void setNombreDiscoteca(String nombreDiscoteca) {
+		this.nombreDiscoteca = nombreDiscoteca;
+	}
+
+}
